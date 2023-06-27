c7cc06a2fe219a6e829de3492c5ee99dff469770
Sheryar Ahmed <136733943+sheryarahmedse@users.noreply.github.com>
2023-06-27 20:09:53 +0500

    removed folder thing and updated with the home api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const {errorHanlder} = require('./middlewares/errorMiddleware.js');
 const connectDB = require('./config/db');
@@ -7,7 +7,10 @@ connectDB();
 const app = express();
 //added as middleware
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
+app.get('/', (req, res) =>{
+    res.send("GOAL SETTER API IS NOW LIVE.");
+})
 //other routes are in getRoutes file
 app.use('/api/goals', require('./routes/getRoutes'));  // for goals
 app.use('/api/users', require('./routes/userRoutes')); // for users
